b1dc331d06a9ba5afe3f0b58ec91fc3a59137769
Dan Kirkham <daniel.a.kirkham@gmail.com>
2024-10-04 18:51:02 -0500

    Generic ConvNum impl

--- src/conv_num.rs
+++ src/conv_num.rs
@@ -4,10 +4,4 @@ use rustfft::FftNum;
 
 pub trait ConvNum: FftNum + ConstZero + NumCast {}
 
-impl ConvNum for f32 {}
-impl ConvNum for f64 {}
-
-// impl ConvNum for i8 {}
-// impl ConvNum for i16 {}
-// impl ConvNum for i32 {}
-// impl ConvNum for i64 {}
+impl<T> ConvNum for T where T: FftNum + ConstZero + NumCast {}
