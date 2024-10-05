5f8f4f1dddd9b526ebb2e8083e52cae55c7bf252
Dan Kirkham <daniel.a.kirkham@gmail.com>
2024-10-04 19:22:54 -0500

    Cleanup input setup code. Create a bench

---
+++ examples/bench.rs
@@ -0,0 +1,25 @@
+use std::time::Instant;
+
+use num_complex::Complex32;
+use rand::prelude::*;
+
+use convolution_dsp::{Conv1dPlanner, ConvMode};
+
+const KERNEL_SIZE: usize = 1024;
+const SIGNAL_SIZE: usize = 1024 * 64;
+const ITERATIONS: usize = 100;
+
+fn main() {
+    let mut rng = rand::thread_rng();
+    let filter: Vec<f32> = (0..KERNEL_SIZE).map(|_| rng.gen_range(-1_f32..1.)).collect();
+
+    let planner = Conv1dPlanner::new();
+    let mut conv = planner.plan_conv1d(&filter, ConvMode::Full);
+
+    let now = Instant::now();
+    for _ in 0..ITERATIONS {
+        let signal: Vec<Complex32> = (0..SIGNAL_SIZE).map(|_| Complex32::new(rng.gen_range(-1_f32..1.), rng.gen_range(-1_f32..1.))).collect();
+        conv.process(signal.clone());
+    }
+    println!("Iterations: {}, Kernel: {}, Signal: {}, Time: {} ms", ITERATIONS, KERNEL_SIZE, SIGNAL_SIZE, now.elapsed().as_millis());
+}
--- src/conv.rs
+++ src/conv.rs
@@ -33,27 +33,24 @@ impl<T: ConvNum> Conv1d<T> {
         }
     }
 
-    pub fn process(&mut self, mut input: Vec<Complex<T>>) -> Vec<Complex<T>> {
-        let input_len = input.len();
+    pub fn process(&mut self, input: Vec<Complex<T>>) -> Vec<Complex<T>> {
         let segment_len = self.fft.len() - self.kernel_len - 1;
-        let segments = ((input_len as f32) / (segment_len as f32)).ceil() as usize;
+        let segments = ((input.len() as f32) / (segment_len as f32)).ceil() as usize;
 
-        // When an N sample signal is convolved with an M sample filter kernel, the output signal
-        // is N + M + 1 samples long.
-        input.extend_from_slice(&vec![Complex::<T>::ZERO; self.kernel_len - 1]);
-
-        let mut output = vec![Complex::<T>::ZERO; input_len + self.kernel_len - 1];
-
-        let needed_len = segments * segment_len;
-        if input_len < needed_len {
-            input.extend_from_slice(&vec![Complex::<T>::ZERO; needed_len - input_len]);
-        }
+        let mut output = vec![Complex::<T>::ZERO; input.len() + self.kernel_len - 1];
 
         let mut segment = Vec::with_capacity(self.fft.len());
         for i in 0..segments {
             let offset = i * segment_len;
-            segment.extend_from_slice(&input[offset..(offset + segment_len)]);
+            let end = offset + segment_len;
+            if end > input.len() {
+                segment.extend_from_slice(&input[offset..input.len()]);
+                segment.extend(vec![Complex::<T>::ZERO; end - input.len()]);
+            } else {
+                segment.extend_from_slice(&input[offset..(offset + segment_len)]);
+            }
             segment.extend(vec![Complex::<T>::ZERO; self.fft.len() - segment_len]);
+            assert_eq!(segment.len(), self.fft.len());
 
             // FFT the segment
             self.fft.process(&mut segment);
@@ -81,7 +78,7 @@ impl<T: ConvNum> Conv1d<T> {
         match self.mode {
             ConvMode::Full => output,
             ConvMode::Same => {
-                let target_len = input_len.max(self.kernel_len);
+                let target_len = input.len().max(self.kernel_len);
                 let left = (output.len() - target_len) / 2;
                 let right = left + target_len;
 
